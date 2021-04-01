3e82caf966707da1c3b2246c5a2b8d2ddfe42792
Baspar <me@bastien-laine.io>
2021-04-01 11:30:31 +0200

    Add DateTime

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import csv
 import numpy
+from datetime import datetime
 
 class Model:
     def _read_route(_self, route_file):
@@ -23,11 +24,26 @@ class Model:
     def _read_wind(_self, wind_file):
         return numpy.loadtxt(wind_file, delimiter=',')
 
+    def _read_datetime(_self, date_file, time_file):
+        with open(date_file, 'r') as date, open(time_file, 'r') as time:
+            date_str = date.readline().strip()
+            year, month, day = date_str[:4], date_str[4:6], date_str[6:8]
+            hour = time.readline().strip()
+            iso_datetime = "{}-{}-{}Z{:0>2}:00:00".format(year, month, day, hour)
+            gmt_datetime = datetime.fromisoformat(iso_datetime)
+            print(gmt_datetime)
+
     def get_route_at_km(self, km):
         rounded_km = int(km)
         return self.route[rounded_km]
 
-    def __init__(self, route_file='route_data.csv', wind_file='interpolated_data/wndgust10m_interp.csv', sun_file='interpolated_data/av_swsfcdown_interp.csv'):
+    def __init__(self,
+            route_file='route_data.csv',
+            wind_file='interpolated_data/wndgust10m_interp.csv',
+            sun_file='interpolated_data/av_swsfcdown_interp.csv',
+            date_file='interpolated_data/base_date_interp.csv',
+            time_file='interpolated_data/base_time_interp.csv'):
         self.route = self._read_route(route_file)
         self.wind = self._read_wind(wind_file)
         self.sun = self._read_sun(sun_file)
+        self.start_datetime = self._read_datetime(date_file, time_file)
--- race_model.py
+++ race_model.py
@@ -5,9 +5,12 @@ class RaceModel:
         self.current_km = 0.0
         self.current_battery = 5000.0
 
-    def move(self, km=0, time=0, speed):
-        if km == 0:
-            km = time * speed
+    def move(self, speed, distance=None, time=None):
+        if distance == None and time == None:
+            raise Exception('Specify a distance or a time')
+
+        distance = distance if distance != None else time * speed
+        time = time if time != None else distance / speed
 
     def rest(self, time):
         return
